943e47f1a40ffe91e66ee25a412f334f1d729662
Jeremy <suurkivi@gmail.com>
2013-07-28 17:42:42 -0700

    Add setup script

---
+++ setup/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+outpin () {
+    PIN=$1
+    echo ${PIN} > /sys/class/gpio/export
+    echo "out" > /sys/class/gpio/gpio${PIN}/direction 
+    chgrp gpio /sys/class/gpio/gpio${PIN}/value
+    chmod g+w /sys/class/gpio/gpio${PIN}/value
+}
+
+outpin 25
+outpin 17
+
+modprobe bcm2708_wdog
+chown root.gpio /dev/watchdog
+chmod g+rw /dev/watchdog
